51d7472440c9c59924e79f5b5d60a569dbb2d4fb
bmilly7 <157335984+bmilly7@users.noreply.github.com>
2025-09-18 21:05:05 -0500

    final corrections and checks

--- help_desk_queue.py
+++ help_desk_queue.py
@@ -3,13 +3,51 @@ from node import Node
 
 # Implement your Queue class here
 class Queue:
-    # Delete the following line and implement your Queue class
-    pass
     
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    
+    def enqueue(self,data):
+        new_node = Node(data)
+
+        if not self.front:
+            self.front = new_node
+            self.rear = new_node
+        else: 
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self): 
+        if not self.front:
+            return None
+        else: 
+            removed_node = self.front
+            self.front = self.front.next
+        if not self.front:
+            self.rear = None
+        return removed_node.data
+
+    def peek(self):
+        if not self.front:
+            return None
+        else:
+            return self.front.data
+
+    def print_queue(self):
+        current = self.front
+        if not current:
+            print("queue is empty")
+        else: 
+            while current:
+                print(f"{current.data}")
+                current = current.next
+
 
 
 def run_help_desk():
     # Create an instance of the Queue class
+    my_queue = Queue()
     
 
     while True:
@@ -24,23 +62,30 @@ def run_help_desk():
         if choice == "1":
             name = input("Enter customer name: ")
             # Add the customer to the queue
-            
+            my_queue.enqueue(name)
             
             print(f"{name} added to the queue.")
         elif choice == "2":
             # Help the next customer in the queue and return message that they were helped
-            pass # delete this line
-
+            helped_customer = my_queue.dequeue()
+            if helped_customer is None:
+                print("no one left. everyone helped!")
+            else: 
+                print(f"next customer was {helped_customer} and they were helped")
 
         elif choice == "3":
             # Peek at the next customer in the queue and return their name
-            pass # delete this line
+            next_customer = my_queue.peek()
+            if next_customer is None:
+                print("no customers next")
+            else:
+                print(f"next customer: {next_customer}")
 
 
         elif choice == "4":
             # Print all customers in the queue
             print("\nWaiting customers:")
-            
+            my_queue.print_queue()
 
         elif choice == "5":
             print("Exiting Help Desk System.")
--- undo_redo_system.py
+++ undo_redo_system.py
@@ -3,11 +3,47 @@ from node import Node
 
 # Implement your Stack class here
 class Stack:
-    pass # delete this line
+    def __init__(self):
+        self.top = None
+
+
+    def push(self,data):
+        new_action = Node(data)
+
+        if not self.top:
+            self.top = new_action
+        else:
+            new_action.next = self.top
+            self.top = new_action
+
+    def pop(self):
+        if not self.top:
+            return None
+        else:
+            popped_node = self.top
+            self.top = self.top.next
+            return popped_node.data
+
+    def print_stack(self):
+        current = self.top
+        if not current:
+            print("stack is empty")
+        else:
+            while current:
+                print(f'{current.data}')
+                current = current.next
+
+
+
+
+
+
+
 
 def run_undo_redo():
     # Create instances of the Stack class for undo and redo
-    
+    undo_stack = Stack()
+    redo_stack = Stack()
 
     while True:
         print("\n--- Undo/Redo Manager ---")
@@ -22,29 +58,41 @@ def run_undo_redo():
         if choice == "1":
             action = input("Describe the action (e.g., Insert 'a'): ")
             # Push the action onto the undo stack and clear the redo stack
+            undo_stack.push(action)
+            redo_stack = Stack()
 
 
             print(f"Action performed: {action}")
         elif choice == "2":
             # Pop an action from the undo stack and push it onto the redo stack
-            pass # delete this line
+            popped = undo_stack.pop()
+            if popped:
+                redo_stack.push(popped)
+                print(f"Undid action: {popped}")
+            else:
+                print("nothing to undo")
             
 
         elif choice == "3":
             # Pop an action from the redo stack and push it onto the undo stack
-            pass # delete this line
+            popped = redo_stack.pop()
+            if popped:
+                undo_stack.push(popped)
+                print(f"redid action: {popped}")
+            else:
+                print("nothing to redo.")
 
 
         elif choice == "4":
             # Print the undo stack
             print("\nUndo Stack:")
-            
+            undo_stack.print_stack()
             
 
         elif choice == "5":
             # Print the redo stack
             print("\nRedo Stack:")
-            
+            redo_stack.print_stack()
             
             
         elif choice == "6":
